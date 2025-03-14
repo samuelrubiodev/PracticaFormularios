7b59e6cd3f87abf264cf763e4feb2d616512356b
samuelrubiodev <samueldynamo11@gmail.com>
2025-03-14 09:02:17 +0100

    Agregado verificaciones en caso de que el usuario haya clicado en los checkboxes menos que el numero de aficiones elegido.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 let userLabel = document.querySelector("#user-label");
+let hobbies_number = document.querySelector("#hobbies-number");
+let aficiones = document.querySelectorAll(".hobbies");
+let botonSubmit = document.querySelector("#boton-submit");
+let mensajeAficiones = document.querySelector(".mensaje-aficiones");
+let fieldSet = document.querySelector(".fieldset");
 
 document.querySelector("#user").addEventListener("input", (event)=>{
     if (event.target.validity.patternMismatch) {
@@ -10,8 +15,6 @@ document.querySelector("#user").addEventListener("input", (event)=>{
     }
 });
 
-let hobbies_number = document.querySelector("#hobbies-number");
-
 hobbies_number.addEventListener("input",(event)=>{
     if (event.target.validity.patternMismatch) {
         event.target.setCustomValidity("Debes elegir un número entre 2 y 4");
@@ -21,25 +24,55 @@ hobbies_number.addEventListener("input",(event)=>{
     }
 });
 
-let aficiones = document.querySelectorAll(".hobbies");
-
 aficiones.forEach(chk => {
-    chk.addEventListener("change", (event)=> {
+    let ultimoActivadas = 0;
+    let activado = false;
+    chk.addEventListener("change", ()=> {
         let numeroAficiones = Number(hobbies_number.value);
+        
         if (cuantasActivadas() == numeroAficiones) {
-            console.log(event.target.checked);
-            desactivarCheckboxs();
-        } 
-    })
-})
+            fieldSet.style.border = "0.1em solid black";
+            mensajeAficiones.textContent = "";
+            desactivarCheckboxes();
+            ultimoActivadas = cuantasActivadas();
+            activado = true;
+        } else if (activado && cuantasActivadas() != ultimoActivadas) {
+            activarCheckboxes();
+            activado = false;
+            ultimoActivadas = 0;
+        }
 
-function desactivarCheckboxs() {
+        if (cuantasActivadas() != numeroAficiones) {
+            fieldSet.style.border = "0.3em dashed red";
+        }
+    })
+});
+
+botonSubmit.addEventListener("click", (event)=>{
+    let numeroAficiones = Number(hobbies_number.value);
+
+    if (cuantasActivadas() < numeroAficiones) {
+        event.preventDefault();
+        mensajeAficiones.textContent = "Debes seleccionar " + numeroAficiones + " aficiones";
+        fieldSet.style.border = "0.3em dashed red";
+    }
+});
+
+function activarCheckboxes() {
+    aficiones.forEach(chk => {
+        if (chk.disabled) {
+            chk.disabled = false;
+        }
+    });
+};
+
+function desactivarCheckboxes() {
     aficiones.forEach(chk => {
         if (!chk.checked) {
             chk.disabled = true;
         }
     });
-}
+};
 
 function cuantasActivadas() {
     let numeroAficiones = 0;
@@ -47,7 +80,6 @@ function cuantasActivadas() {
         if (chk.checked) {
             numeroAficiones++;
         }
-    })
-    console.log(numeroAficiones)
+    });
     return numeroAficiones;
 };
